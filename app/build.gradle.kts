843d9414d08312c98bd45aa83195a214d78cae0e
mayeem007 <moyeemmd@gmail.com>
2025-07-26 00:33:26 +0530

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,25 +34,20 @@ android {
         jvmTarget = "11"
     }
     buildFeatures {
-        compose = false // Set to true ONLY if using Jetpack Compose UI
+        compose = false
     }
 }
 
 dependencies {
-    // Core Android UI, Material Design, and Jetpack libraries
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.activity:activity-ktx:1.7.2")
-
-    // ExoPlayer (video player; you do NOT need the non-existent extension-downloader)
+    // ExoPlayer for video playback
     implementation("com.google.android.exoplayer:exoplayer:2.18.5")
     implementation("com.google.android.exoplayer:extension-okhttp:2.18.5")
-
-    // Testing
+    // Unit Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
-
-
