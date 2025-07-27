1394f3ff592a09671b40c7fabfb89bf7ba00ad61
mayeem007 <moyeemmd@gmail.com>
2025-07-28 04:01:59 +0530

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,9 +58,10 @@ dependencies {
     implementation(libs.androidx.datastore.preferences)
 
     // Media3 - Use consistent version
-    implementation("androidx.media3:media3-exoplayer:1.7.1")
-    implementation("androidx.media3:media3-ui:1.7.1")
-    implementation("androidx.media3:media3-downloader:1.7.1")
+    implementation(libs.androidx.media3.exoplayer.v171)
+    implementation(libs.androidx.media3.ui.v171)
+    //noinspection UseTomlInstead
+    implementation(libs.androidx.media3.downloader.v171)
 
     // Hilt
     implementation(libs.dagger.hilt.android)
